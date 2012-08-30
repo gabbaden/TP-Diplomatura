ceae7372137b954ba661879b4256fa5760b63fb9
baden <baden@baden-N68S3.(none)>
2012-08-30 12:57:10 -0300

    Agregados los alumnos y la institucion

---
+++ src/modelo/Alumno.java
@@ -0,0 +1,50 @@
+package modelo;
+
+public class Alumno {
+
+	private String nombre;
+	private String apellido;
+	private String email;
+	private int nota;
+	private String curso;
+	private String estado;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getNota() {
+		return nota;
+	}
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+}
---
+++ src/modelo/Institucion.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class Institucion {
+
+	private String institucion;
+	private String cursoFinalizado;
+	private int descuento;
+	
+	
+	public String getInstitucion() {
+		return institucion;
+	}
+	public void setInstitucion(String institucion) {
+		this.institucion = institucion;
+	}
+	public String getCursoFinalizado() {
+		return cursoFinalizado;
+	}
+	public void setCursoFinalizado(String cursoFinalizado) {
+		this.cursoFinalizado = cursoFinalizado;
+	}
+	public int getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(int descuento) {
+		this.descuento = descuento;
+	}
+	
+}
