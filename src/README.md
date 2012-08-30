d05618542b642c74f1c547a8c5ae86b7d696ed6b
baden <baden@baden-N68S3.(none)>
2012-08-30 12:37:10 -0300

    cambios en el controlador, agregando metodos basicos

---
+++ src/README.md
@@ -0,0 +1,6 @@
+TP-Diplomatura
+==============
+
+Primer trabajo diplomatura Java
+
+Por ahora vamos a trabajar sin base de datos, vamos a persistir los datos en memoria.
--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -9,5 +9,32 @@ public class Controlador {
 		// TODO Auto-generated method stub
 
 	}
+	
+	
+	//Cursos que actualmente se están cursando por categoría.
+	public void cursosQueSeCursandoxCategoria(){
+		
+	}
+	
+	//Nombres de alumnos, categorías de cursos de interés y correos de contacto.
+	public void datosAlumnos(){
+		
+	}
+	
+	//Promedio de las notas de los alumnos ya sea por curso,categoría o en general. 
+	//El orden debe ser por promedio.
+	public void promedio(){
+		
+	}
+	
+	//Cálculo de las cuotas a pagar por alumno y listado ordenado por abecedario.
+	public void calculoCuatoAlumno(){
+		
+	}
+	//Cálculo del monto a pagar por cada curso, listando todos los alumnos 
+	//unificados bajo “Particular” pero distinguiendo cada empresa en particular.
+	public void montoCadaCursoListadoAlumno(){
+		
+	}
 
 }
