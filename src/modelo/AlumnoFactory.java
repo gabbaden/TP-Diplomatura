668c4e31d8dc84e32e68252bd5e616ad51064949
jackgris <jackgris2@gmail.com>
2012-09-06 16:53:33 -0300

    cambie la clase modelo para trabajar con listas y empece a ver lo que va a ser la factory de alumnos

--- src/modelo/AlumnoFactory.java
+++ src/modelo/AlumnoFactory.java
@@ -1,14 +1,33 @@
 package modelo;
 
-
+import java.util.Iterator;
+import modelo.Modelo;
 
 /**
  * @author jackgris
  * Clase factory para crear ir creando a los alumnos que vamos a utilizar
+ * 
  *
  */
 public class AlumnoFactory {
 
+	Modelo datos = new Modelo();
 	
+	public Alumno alumnoFactory(){
+		
+		Iterator<String> iter = datos.nombres.iterator();
+		
+		while(iter.hasNext()){
+		
+//			if(((String) datos).estados(1) != null){
+//				return new AlumnoComun();					
+//			}
+//			else{
+//				return new AlumnoRegular();
+//			}  mirando como crear los alumnos
+		}
+		return null;
+		
+	}
 	
 }
--- src/modelo/Modelo.java
+++ src/modelo/Modelo.java
@@ -1,41 +1,143 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Modelo {
 	
-	String[] nombres = {"Esteban", "Julio", "Ernesto", "Andres", "Victor","Marcelo",
-			"Pepe","Jose","Michael","Julio","Julio","Miguel","Marcelo","Roberto"};
-	
-	String[] apellidos = {"Quito","Cambiaso","Lujan","Farias","Calzado","Bochini",
-			"Arena","Uzuriaga","Arena","Aguero","Cambiasso","Denis","Bochini","Pusineri"};
-	
-	String[] emails = {"estequit@gmail.com","camb@arnet.com","ernest@huy.com","key@map.com.ar",
-			"naic@naic.com","bocha2@bocha.com","eee@gmail.com","pajar@uro.net","ddd@gmail.com",
-			"gol@hotmail.com", "camb@arnet.com","mike_sd@gmail.com","bocha1@bocha.com",
-			"pusi_gol@gol.com"};
-	
-	Double [] notas = {9.0,null,8.5,7.5,null,null,1.5,7.0,4.0,9.5,5.0,8.5,10.0,null};
-	
-	String [] cursos = {"Java B", "Java A","Java A","Net Web","Net Web","Java Web","Net B",
-			"Net B","Net B","Java Web","Java B","Net B","Java A","Java B"};
-	
-	String [] estado = {"Finalizado","Abandonado","Finalizado","Finalizado","Cursando","Cursando",
-			"Finalizado","Finalizado","Finalizado","Finalizado","Finalizado","Finalizado",
-			"Finalizado","Cursando"};
-	
-	String [] institucion = {"La Paz Instituto","ISET 34","ISET 44","ISET 34","Fundación Bocha",
-			"La Paz Instituto","La Paz Instituto"};
-	
-	String [] cursoFinalizado = {"Java B","Net B","Net A","Net B","Java Web","Java A","Java Web"};
-	
-	Integer[] descuento = {10,4,15,4,20,10,10};
-	
+	public List<String> nombres = new ArrayList<String>();
+	public List<String> apellidos = new  ArrayList<String>();
+	public List<String> emails = new  ArrayList<String>();
+	public List<Double> notas = new ArrayList<Double>();
+	public List<String> cursos = new  ArrayList<String>();
+	public List<String> estados = new  ArrayList<String>();
+	public List<String> instituciones = new  ArrayList<String>();
+	public List<String> cursoFinalizado = new  ArrayList<String>();
+	public List<Integer> descuentos = new ArrayList<Integer>();
+		
 	
 	public void queHace(){
 		System.out.println("Tengo que ver la relacion entre todos los objetos" +
 				" en el modelo");
 	}
 
+	private void cargarDatos (){
+
+		nombres.add("Esteban");
+		nombres.add("Julio");
+		nombres.add("Ernesto");
+		nombres.add("Andres");
+		nombres.add("Victor");
+		nombres.add("Marcelo");
+		nombres.add("Pepe");
+		nombres.add("Jose");
+		nombres.add("Michael");
+		nombres.add("Julio");
+		nombres.add("Julio");
+		nombres.add("Miguel");
+		nombres.add("Marcelo");
+		nombres.add("Roberto");
+			
+		apellidos.add("Quito");
+		apellidos.add("Cambiaso");
+		apellidos.add("Lujan");
+		apellidos.add("Farias");
+		apellidos.add("Calzado");
+		apellidos.add("Bochini");
+		apellidos.add("Arena");
+		apellidos.add("Uzuriaga");
+		apellidos.add("Arena");
+		apellidos.add("Aguero");
+		apellidos.add("Cambiasso");
+		apellidos.add("Denis");
+		apellidos.add("Bochini");
+		apellidos.add("Pusineri");
+			
+		emails.add("estequit@gmail.com");
+		emails.add("camb@arnet.com");
+		emails.add("ernest@huy.com");
+		emails.add("key@map.com.ar");
+		emails.add("naic@naic.com");
+		emails.add("bocha2@bocha.com");
+		emails.add("eee@gmail.com");
+		emails.add("pajar@uro.net");
+		emails.add("ddd@gmail.com");
+		emails.add("gol@hotmail.com");
+		emails.add("camb@arnet.com");
+		emails.add("mike_sd@gmail.com");
+		emails.add("bocha1@bocha.com");
+		emails.add("pusi_gol@gol.com");
+			
+		notas.add(9.0);
+		notas.add(null);
+		notas.add(8.5);
+		notas.add(7.5);
+		notas.add(null);
+		notas.add(null);
+		notas.add(1.5);
+		notas.add(7.0);
+		notas.add(4.0);
+		notas.add(9.5);
+		notas.add(5.0);
+		notas.add(8.5);
+		notas.add(10.0);
+		notas.add(null);
+		
+		cursos.add("Java B");
+		cursos.add("Java A");
+		cursos.add("Java A");
+		cursos.add("Net Web");
+		cursos.add("Net Web");
+		cursos.add("Java Web");
+		cursos.add("Net B");
+		cursos.add("Net B");
+		cursos.add("Net B");
+		cursos.add("Java Web");
+		cursos.add("Java B");
+		cursos.add("Net B");
+		cursos.add("Java A");
+		cursos.add("Java B");
+			
+		estados.add("Finalizado");
+		estados.add("Abandonado");
+		estados.add("Finalizado");
+		estados.add("Finalizado");
+		estados.add("Cursando");
+		estados.add("Cursando");
+		estados.add("Finalizado");
+		estados.add("Finalizado");
+		estados.add("Finalizado");
+		estados.add("Finalizado");
+		estados.add("Finalizado");
+		estados.add("Finalizado");
+		estados.add("Finalizado");
+		estados.add("Cursando");
+	
+		instituciones.add("La Paz Instituto");
+		instituciones.add("ISET 34");
+		instituciones.add("ISET 44");
+		instituciones.add("ISET 34");
+		instituciones.add("Fundación Bocha");
+		instituciones.add("La Paz Instituto");
+		instituciones.add("La Paz Instituto");
+
+		cursoFinalizado.add("Java B");
+		cursoFinalizado.add("Net B");
+		cursoFinalizado.add("Net A");
+		cursoFinalizado.add("Net B");
+		cursoFinalizado.add("Java Web");
+		cursoFinalizado.add("Java A");
+		cursoFinalizado.add("Java Web");
+			
+		descuentos.add(10);
+		descuentos.add(4);
+		descuentos.add(15);
+		descuentos.add(4);
+		descuentos.add(20);
+		descuentos.add(10);
+		descuentos.add(10);
+		
+		}
 	
 }
